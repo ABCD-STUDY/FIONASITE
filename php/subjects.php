e3ba6e357c6ff7aef690cbce6c191433884ac43f
Hauke Bartsch <HaukeBartsch@gmail.com>
2015-08-04 19:55:31 -0700

    update

--- php/subjects.php
+++ php/subjects.php
@@ -6,9 +6,15 @@
   $subjects = array();
   foreach ($results as $result) {
     if ($result === '.' or $result === '..') continue;
-
-    if (is_dir($path . '/' . $result)) {
-        $subjects[] = $path . '/'. $result;
+    if (is_dir($d . '/' . $result)) {
+        $files = glob($d . '/' . $result . '/*.json');
+	foreach ($files as $f) {
+  	  $data = json_decode(file_get_contents($files[0]), True);
+	  if ( array_key_exists('PatientID', $data) ) {
+            $subjects[] = $data['PatientID'];
+	    break; // only check the first non-empty patient ID
+	  }
+	}
     }
   }
   echo(json_encode($subjects, JSON_PRETTY_PRINT));
