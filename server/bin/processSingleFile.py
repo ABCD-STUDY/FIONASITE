6b1a2d90873bc90def982d8e11e0016ae5047d8f
Hauke Bartsch <HaukeBartsch@gmail.com>
2016-03-04 03:05:38 -0500

    read the Siemens CSA header information

--- server/bin/processSingleFile.py
+++ server/bin/processSingleFile.py
@@ -6,9 +6,11 @@ extracts the header information and creates a Study/Series symbolic link structu
 
 import sys, os, time, atexit, stat, tempfile, copy
 import dicom, json, re, logging, logging.handlers, threading
+import struct
 from signal import SIGTERM
 from dicom.filereader import InvalidDicomError
 
+ASSERTIONS_ENABLED = False
 
 class Daemon:
         """
@@ -255,6 +257,195 @@ class ProcessSingleFile(Daemon):
 			print("Error: tag with unknown structure, should be 1, 2, or 3 entries in array")
                         logging.error("Error: tag with unknown structure, should be 1, 2, or 3 entries in array")
 		return taghere, v
+
+        def _null_truncate(self, s):
+                """Given a string, returns a version truncated at the first '\0' if
+                there is one. If not, the original string is returned."""
+                i = s.find(chr(0))
+                if i != -1:
+                        s = s[:i]
+                        
+                return s
+
+
+        def _scrub(self, item):
+                """Given a string, returns a version truncated at the first '\0' and
+                stripped of leading/trailing whitespace. If the param is not a string,
+                it is returned unchanged."""
+                if isinstance(item, basestring):
+                        return self._null_truncate(item).strip()
+                else:
+                        return item
+
+
+        def _get_chunks(self, tag, index, format, little_endian=True):
+                """Given a CSA tag string, an index into that string, and a format
+                specifier compatible with Python's struct module, returns a tuple
+                of (size, chunks) where size is the number of bytes read and
+                chunks are the data items returned by struct.unpack(). Strings in the
+                list of chunks have been run through _scrub().
+                """
+                # The first character of the format string indicates endianness.
+                format = ('<' if little_endian else '>') + format
+                size = struct.calcsize(format)
+                chunks = struct.unpack(format, tag[index:index + size])
+                
+                chunks = [self._scrub(item) for item in chunks]
+        
+                return (size, chunks)
+
+        def _my_assert(self, expression):
+                if ASSERTIONS_ENABLED:
+                        assert(expression)
+
+        def _get(self, dataset, tag, default=None):
+                """Returns the value of a dataset tag, or the default if the tag isn't
+                in the dataset.
+                PyDicom datasets already have a .get() method, but it returns a
+                dicom.DataElement object. In practice it's awkward to call dataset.get()
+                and then figure out if the result is the default or a DataElement,
+                and if it is the latter _get the .value attribute. This function allows
+                me to avoid all that mess.
+                It is also a workaround for this bug (which I submitted) which should be
+                fixed in PyDicom > 0.9.3:
+                http://code.google.com/p/pydicom/issues/detail?id=72
+                Also for this bug (which I submitted) which should be
+                fixed in PyDicom > 0.9.4-1:
+                http://code.google.com/p/pydicom/issues/detail?id=88
+                """
+                return default if tag not in dataset else dataset[tag].value
+
+        def _parse_csa_header(self, tag, little_endian = True):
+                """The CSA header is a Siemens private tag that should be passed as
+                a string. Any of the following tags should work: (0x0029, 0x1010),
+                (0x0029, 0x1210), (0x0029, 0x1110), (0x0029, 0x1020), (0x0029, 0x1220),
+                (0x0029, 0x1120).
+                
+                The function returns a dictionary keyed by element name.
+                """
+                # Let's have a bit of fun, shall we? A Siemens CSA header is a mix of
+                # binary glop, ASCII, binary masquerading as ASCII, and noise masquerading
+                # as signal. It's also undocumented, so there's no specification to which
+                # to refer.
+                
+                # The format is a good one to show to anyone who complains about XML being
+                # verbose or hard to read. Spend an afternoon with this and XML will
+                # look terse and read like a Shakespearean sonnet.
+                
+                # The algorithm below is a translation of the GDCM project's
+                # CSAHeader::LoadFromDataElement() inside gdcmCSAHeader.cxx. I don't know
+                # how that code's author figured out what's in a CSA header, but the
+                # code works.
+                
+                # I added comments and observations, but they're inferences. I might
+                # be wrong. YMMV.
+                
+                # Some observations --
+                # - If you need to debug this code, a hexdump of the tag data will be
+                #   your best friend.
+                # - The data in the tag is a list of elements, each of which contains
+                #   zero or more subelements. The subelements can't be further divided
+                #   and are either empty or contain a string.
+                # - Everything begins on four byte boundaries.
+                # - This code will break on big endian data. I don't know if this data
+                #   can be big endian, and if that's possible I don't know what flag to
+                #   read to indicate that. However, it's easy to pass an endianness flag
+                #   to _get_chunks() should the need to parse big endian data arise.
+                # - Delimiters are thrown in here and there; they are 0x4d = 77 which is
+                #   ASCII 'M' and 0xcd = 205 which has no ASCII representation.
+                # - Strings in the data are C-style NULL terminated.
+                
+                # I sometimes read delimiters as strings and sometimes as longs.
+                DELIMITERS = ("M", "\xcd", 0x4d, 0xcd)
+
+                # This dictionary of elements is what this function returns
+                elements = { }
+
+                # I march through the tag data byte by byte (actually a minimum of four
+                # bytes at a time), and current points to my current position in the tag
+                # data.
+                current = 0
+
+                # The data starts with "SV10" followed by 0x04, 0x03, 0x02, 0x01.
+                # It's meaningless to me, so after reading it, I discard it.
+                size, chunks = self._get_chunks(tag, current, "4s4s")
+                current += size
+
+                self._my_assert(chunks[0] == "SV10")
+                self._my_assert(chunks[1] == "\4\3\2\1")
+
+                # get the number of elements in the outer list
+                size, chunks = self._get_chunks(tag, current, "L")
+                current += size
+                element_count = chunks[0]
+                
+                # Eat a delimiter (should be 0x77)
+                size, chunks = self._get_chunks(tag, current, "4s")
+                current += size
+                self._my_assert(chunks[0] in DELIMITERS)
+
+                for i in range(element_count):
+                        # Each element looks like this:
+                        # - (64 bytes) Element name, e.g. ImagedNucleus, NumberOfFrames,
+                        #   VariableFlipAngleFlag, MrProtocol, etc. Only the data up to the
+                        #   first 0x00 is important. The rest is helpfully populated with
+                        #   noise that has enough pattern to make it look like something
+                        #   other than the garbage that it is.
+                        # - (4 bytes) VM
+                        # - (4 bytes) VR
+                        # - (4 bytes) syngo_dt
+                        # - (4 bytes) # of subelements in this element (often zero)
+                        # - (4 bytes) a delimiter (0x4d or 0xcd)
+                        size, chunks = self._get_chunks(tag, current,
+                                                   "64s" + "4s" + "4s" + "4s" + "L" + "4s")
+                        current += size
+
+                        name, vm, vr, syngo_dt, subelement_count, delimiter = chunks
+                        self._my_assert(delimiter in DELIMITERS)
+
+                        # The subelements hold zero or more strings. Those strings are stored
+                        # temporarily in the values list.
+                        values = [ ]
+                        
+                        for j in range(subelement_count):
+                                # Each subelement looks like this:
+                                # - (4 x 4 = 16 bytes) Call these four bytes A, B, C and D. For
+                                #   some strange reason, C is always a delimiter, while A, B and
+                                #   D are always equal to one another. They represent the length
+                                #   of the associated data string.
+                                # - (n bytes) String data, the length of which is defined by
+                                #   A (and A == B == D).
+                                # - (m bytes) Padding if length is not an even multiple of four.
+                                size, chunks = self._get_chunks(tag, current, "4L")
+                                current += size
+                                
+                                self._my_assert(chunks[0] == chunks[1])
+                                self._my_assert(chunks[1] == chunks[3])
+                                self._my_assert(chunks[2] in DELIMITERS)
+                                length = chunks[0]
+
+                                # get a chunk-o-stuff, length indicated by code above.
+                                # Note that length can be 0.
+                                size, chunks = self._get_chunks(tag, current, "%ds" % length)
+                                current += size
+                                if chunks[0]:
+                                        values.append(chunks[0])
+                                        
+                                # If we're not at a 4 byte boundary, move.
+                                # Clever modulus code below swiped from GDCM
+                                current += (4 - (length % 4)) % 4
+
+                        # The value becomes a single string item (possibly "") or a list
+                        # of strings
+                        if len(values) == 0:
+                                values = ""
+                        if len(values) == 1:
+                                values = values[0]
+                        
+                        self._my_assert(name not in elements)
+                        elements[name] = values
+
+                return elements
 			
         def classify(self,dataset,data,classifyTypes):
                 # read the classify rules
@@ -419,6 +610,32 @@ class ProcessSingleFile(Daemon):
                                         print("Not a DICOM file: ", response)
                                         logging.error('Not a DICOM file: %s' % response)
                                         continue
+                                # if we have a Siemens file get the CSA header structure as well
+                                # (0x0029, 0x__10) is one of several possibilities
+                                # - SIEMENS CSA NON-IMAGE, CSA Data Info
+                                # - SIEMENS CSA HEADER, CSA Image Header Info
+                                # - SIEMENS CSA ENVELOPE, syngo Report Data
+                                # - SIEMENS MEDCOM HEADER, MedCom Header Info
+                                # - SIEMENS MEDCOM OOG, MedCom OOG Info (MEDCOM Object Oriented Graphics)
+                                # Pydicom identifies it as "CSA Image Header Info"
+                                for tag in ( (0x0029, 0x1010), (0x0029, 0x1210), (0x0029, 0x1110) ):
+                                        tag_data = None if tag not in dataset else dataset[tag].value
+                                        if tag_data:
+                                                break
+                                                
+                                if tag_data:
+                                        ptag_img = self._parse_csa_header(tag_data)
+
+                                # [IDL] Access the SERIES Shadow Data
+                                # [PS] I don't know what makes this "shadow" data.
+                                for tag in ( (0x0029, 0x1020), (0x0029, 0x1220), (0x0029, 0x1120) ):
+                                        tag_data = self._get(dataset, tag, None)
+                                if tag_data:
+                                        break
+                                        
+                                if tag_data:
+                                        ptag_ser = self._parse_csa_header(tag_data)
+
                                 arriveddir = '/data/site/.arrived'
                                 if not os.path.exists(arriveddir):
                                         os.makedirs(arriveddir)
