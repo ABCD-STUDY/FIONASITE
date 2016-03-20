9eecd36a075af3cc72eba9ea934978e206e32e4d
Hauke Bartsch <HaukeBartsch@gmail.com>
2016-03-20 10:17:41 -0700

    prevent error message in apache log

--- php/stats.php
+++ php/stats.php
@@ -5,7 +5,8 @@ function getSystemMemInfo()
     $data = explode("\n", file_get_contents("/proc/meminfo"));
     $meminfo = array();
     foreach ($data as $line) {
-    	    list($key, $val) = explode(":", $line);
+            // some lines will not contain a colon, for those add one at the end
+    	    list($key, $val) = explode(":", "${line}:");
     	    $meminfo[$key] = trim($val);
     }
     return $meminfo;
