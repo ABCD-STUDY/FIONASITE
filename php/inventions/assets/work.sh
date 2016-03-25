e0fed5f55dd0f3a72e415546adc7ab81920f992d
Hauke Bartsch <HaukeBartsch@gmail.com>
2016-03-24 17:15:42 -0700

    no variables in this script yet

--- php/inventions/assets/work.sh
+++ php/inventions/assets/work.sh
@@ -2,15 +2,18 @@
 if [ $# -ne 2 ];
 then
    echo "NAME:"
-   echo "  created by buckets (`date +"%D"`)"
+   echo "  created by inventions"
    echo "USAGE:"
    echo ""
-   echo "  work.sh <dicom directory> <output directory>"
+   echo "  work.sh <input dicom directory> <output directory>"
    exit; 
 fi
+
 input="$1"
 output="$2"
+
 # Add the call to your installed program here.
 #    ./example_program "${input}" "${output}"
 # Any result should be copied to ${output}. Don't change/delete anything in ${input}.
+
 echo "Done..."
