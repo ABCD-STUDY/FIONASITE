216c7170c31c50963ce03a165cc302a71fda74bf
Alex DeCastro <alexdecastro2@gmail.com>
2016-06-28 16:18:18 -0400

    only show the send button if a file is in quarantine

--- php/fileStatus.php
+++ php/fileStatus.php
@@ -19,8 +19,8 @@ $path_info = pathinfo($filename);
 // we can find this study/series in three locations
 // we will assume that the naming convention ensures that the series instance uid is in the filename
 $q = glob('/data/quarantine/'.$path_info['filename'].".".$path_info['extension']);
-$o = glob('/data/outbox/'.$path_info['filename'].".".$path_info['extension']);
-$d = glob('/data/DAIC/'.$path_info['filename'].".".$path_info['extension']);
+$o = glob('/data/outbox/*'.$path_info['filename'].".".$path_info['extension']);
+$d = glob('/data/DAIC/*'.$path_info['filename'].".".$path_info['extension']);
 
 // we should check if we have an md5sum file for each one
 $qvalid = array();
@@ -70,65 +70,5 @@ foreach($dvalid as $qv) {
 echo(json_encode($val));
 
 return;
-
-if ($handle = opendir('/data/quarantine')) {
-    $found = false;
-    // iterate the files in the directory
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != "..") {
-            if(!strstr($file, $study)) {
-                continue; // skip the files if it does not contain $study
-            }
-            $found = true;
-        }
-    }
-    /*Close the handle*/
-    closedir($handle);
-    if ($found) {
-        echo ("{ \"ok\": 1, \"message\": \"Ready to send\" }");
-        return;
-    }
-}
-
-if ($handle = opendir('/data/outbox')) {
-    $found = false;
-    // iterate the files in the directory
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != "..") {
-            if(!strstr($file, $study)) {
-                continue; // skip the files if it does not contain $study
-            }
-            $found = true;
-        }
-    }
-    /*Close the handle*/
-    closedir($handle);
-    if ($found) {
-        echo ("{ \"ok\": 2, \"message\": \"Pending send to DAIC\" }");
-        return;
-    }
-}
-
-if ($handle = opendir('/data/DAIC')) {
-    $found = false;
-    // iterate the files in the directory
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != "..") {
-            if(!strstr($file, $study)) {
-                continue; // skip the files if it does not contain $study
-            }
-            $found = true;
-        }
-    }
-    /*Close the handle*/
-    closedir($handle);
-    if ($found) {
-        echo ("{ \"ok\": 3, \"message\": \"Sent to DAIC\" }");
-        return;
-    } else {
-        echo ("{ \"ok\": 0, \"message\": \"ERROR: study not found\" }");
-        return;
-    }
-}
 
 ?>
