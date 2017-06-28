b5d7179c3d60c308a5abc61de579ad8d0737e8d9
Hauke Bartsch <HaukeBartsch@gmail.com>
2017-06-28 12:08:36 -0600

    add cleaning of FIRMM directory as well - default age value is 120 days

--- server/utils/cleanDrive.sh
+++ server/utils/cleanDrive.sh
@@ -3,10 +3,10 @@
 #
 # Delete transferred data entries that are older than 120 days from the FIONA system.
 # Only data that has been send already to the DAIC will be deleted (size set to zero).
-#
+# (run as root's cron)
 # 0 */23 * * * /var/www/html/server/utils/cleanDrive.sh >> /var/www/html/server/logs/cleanDrive.log
 
-find /data/DAIC -name "*.tgz" -type f -mtime +120 -print0 | while read -d $'\0' file
+find /data/DAIC -name "*.tgz" -type f -mtime +50 -print0 | while read -d $'\0' file
 do
    # check amount of free space and quit if there is still enough space left (computed in megabytes 1 963 374)
    FREE=`df --output=avail -h "/data/site/archive/" -m | sed '1d;s/[^0-9]//g'`
@@ -20,4 +20,24 @@ do
    touch -d "$STAMP" "$file"
    echo "`date`: truncate $file to size 0"
    #ls -lah $file
+done
+
+find /data/site/scanner-share -type f -mtime +120 -print0 | while read -d $'\0' file
+do
+   # check amount of free space and quit if there is still enough space left (computed in megabytes 1 963 374)
+   FREE=`df --output=avail -h "/data/site/archive/" -m | sed '1d;s/[^0-9]//g'`
+   # lets keep at least 3TB free on the disk
+   if [[ $FREE -gt 3000000 ]]; then
+      echo "`date`: enough ($FREE) memory available on this system"
+      exit;
+   fi
+   if [ -s "$file" ]; then
+      # will be executed by root so we can become someone else here (owner of data in /data/site/scanner-share)
+      su -s /usr/bin/bash -c "/usr/bin/rm -- $file" nobody
+      if [ ! -f "$file" ]; then
+         echo "`date`: removed $file"
+      else
+         echo "`date`: error, count not remove $file"
+      fi
+   fi
 done
