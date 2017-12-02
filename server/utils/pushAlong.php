fc60c6ec2213ec55cfdf6e96633f8c971568450d
Hauke Bartsch <HaukeBartsch@gmail.com>
2017-12-02 12:56:50 -0700

    extend dataflow and pushAlong for other projects

--- server/utils/pushAlong.php
+++ server/utils/pushAlong.php
@@ -14,14 +14,19 @@
       exit(1);
    }
 
-   $shortopts = "f";
+   $shortopts = "fp:";
    $options = getopt($shortopts);
    $force = false;
    if (isset($options['f'])) {
       $force = true;
    } else {
       $force = false;
-   } 
+   }
+   $project = ""; 
+   if (isset($options['p'])) {
+      $project = $options['p'];
+      echo("Switch to project: \"" . $project."\"\n");
+   }
 
    $config = json_decode(file_get_contents("/data/config/config.json"), TRUE);
    if (isset($config['LOCALTIMEZONE'])) {
@@ -35,7 +40,11 @@
    $fname = '/var/www/html/php/getDataFlow.php';
    $data = null;
    if (is_readable($fname)) {
-      $data = exec('/usr/bin/php '.$fname);
+      $p_name = "";
+      if ($project !== "") {
+         $p_name = " -p ".$project;
+      }
+      $data = exec('/usr/bin/php '.$fname.$p_name);
       $data = json_decode($data, true);
    } else {
      echo("File not readable ".$fname);
@@ -51,13 +60,21 @@
    foreach($data as $key => $study) {
       if (array_key_exists('archive', $study) && $study['archive'] == 1 && !array_key_exists('raw',$study)) {
          echo ("found study in archive that is not in raw ".$key."\n");
-         $path = '/data/site/archive/scp_'.$key;
+         $path = '/data'.$project.'/site/archive/scp_'.$key;
          if (is_dir($path)) {
   	    if ($force) {
   	       // send these images using s2m.sh
-	       exec('/var/www/html/server/utils/s2m.sh '.$path);
+	       if ($project != "") {
+  	          exec('/var/www/html/server/utils/s2m_'.$project.'.sh '.$path);
+               } else {
+  	          exec('/var/www/html/server/utils/s2m.sh '.$path);
+               }
             } else {
-	       echo (" run: /var/www/html/server/utils/s2m.sh ".$path."\n");
+               if ($project == "") {
+  	         echo (" run: /var/www/html/server/utils/s2m.sh ".$path."\n");
+	       } else {
+  	         echo (" run: /var/www/html/server/utils/s2m_".$project.".sh ".$path."\n");
+               }
 	    }
          } else {
 	    echo (" Error: did not find one archive that fits ".$path."\n");
@@ -81,7 +98,7 @@
             continue;
 
 	 // if we have an entry in DAIC for this study, we should try to get the correct header
-	 $daics = glob('data/DAIC/*'.$studyInstanceUID.'*');
+	 $daics = glob('data'.$project.'/DAIC/*'.$studyInstanceUID.'*');
 	 $header = "";
 	 if ( count($daics) > 0 ) {
 	    // try to find a good header from these image series - only if we find a good one continue
@@ -118,14 +135,14 @@
 	    // This should be a move/rename from quarantine to outbox for tgz,md5sum and json
 	    foreach($quarantineButNotOutboxOrDAIC as $seriesInstanceUID => $value) {
 	       // all files present?
-	       $tgz    = glob('data/quarantine/*'.$seriesInstanceUID.'*.tgz');
-	       $md5sum = glob('data/quarantine/*'.$seriesInstanceUID.'*.md5sum');
-	       $json   = glob('data/quarantine/*'.$seriesInstanceUID.'*.json');
+	       $tgz    = glob('data'.$project.'/quarantine/*'.$seriesInstanceUID.'*.tgz');
+	       $md5sum = glob('data'.$project.'/quarantine/*'.$seriesInstanceUID.'*.md5sum');
+	       $json   = glob('data'.$project.'/quarantine/*'.$seriesInstanceUID.'*.json');
 	       if (count($tgz) > 0 && count($md5sum) > 0 && count($json) > 0 &&
 	       	  count($tgz) == count($md5sum) && count($md5sum) == count($json) &&
 		  count($tgz) < 3 // should be maximally a DICOM and a k-space tgz for a single series
 	       ) {
-	          if (!is_writable('data/quarantine') || !is_writable('data/outbox')) {
+	          if (!is_writable('data'.$project.'/quarantine') || !is_writable('data'.$project.'/outbox')) {
 		     echo ("ERROR: we cannot write into either the source or destination. Permissions wrong?");
 		     continue;
 		  }
@@ -139,7 +156,7 @@
 	          $ok = true;
 		  foreach($tgz as $t) {
 		      $path = pathinfo($t);
-		      $destination = "data/outbox/".$header.$path['filename'].".".$path['extension'];
+		      $destination = "data".$project."/outbox/".$header.$path['filename'].".".$path['extension'];
  		      if ($ok) {
 		         $ok = copy($t, $destination);
 		         if ($ok) { 
@@ -154,7 +171,7 @@
 		  }
 		  foreach($md5sum as $t) {
 		      $path = pathinfo($t);
-		      $destination = "data/outbox/".$header.$path['filename'].".".$path['extension'];
+		      $destination = "data".$project."/outbox/".$header.$path['filename'].".".$path['extension'];
  		      if ($ok) {
 		         $ok = copy($t, $destination);
 		         if ($ok) { 
@@ -169,7 +186,7 @@
 		  }
 		  foreach($json as $t) {
 		      $path = pathinfo($t);
-		      $destination = "data/outbox/".$header.$path['filename'].".".$path['extension'];
+		      $destination = "data".$project."/outbox/".$header.$path['filename'].".".$path['extension'];
  		      if ($ok) {
 		         $ok = copy($t, $destination);
 		         if ($ok) { 
@@ -204,9 +221,9 @@
 	    if (array_key_exists('DAIC',$v) && $v['DAIC'] == 1 &&
 	        array_key_exists('quarantine',$v) && $v['quarantine'] == 1) {
 		// check the file times of both tgz files
-		$tgzDAIC = glob('data/DAIC/*'.$seriesInstanceUID.'*.tgz');
+		$tgzDAIC = glob('data'.$project.'/DAIC/*'.$seriesInstanceUID.'*.tgz');
 		for ( $i = 0; $i < count($tgzDAIC); $i++) { // we want to find the correct DAIC file (matches with the series we found in quarantine)
-		    $tgzQuarantine = glob('data/quarantine/*'.$seriesInstanceUID.'*.tgz');
+		    $tgzQuarantine = glob('data'.$project.'/quarantine/*'.$seriesInstanceUID.'*.tgz');
 		    for ( $j = 0; $j < count($tgzQuarantine); $j++) {
 		        if ( filemtime($tgzDAIC[$i]) < filemtime($tgzQuarantine[$j]) ) {
 		           $fn = basename($tgzDAIC[$i],'.tgz');
@@ -227,22 +244,22 @@
 		           $ofn = basename($tgzQuarantine[$j],'.tgz');
 
 			   // our filename we would like to copy to /data/DAIC/ would be (prepend our header):
-			   $newfilename = '/data/DAIC/' . $header . '_' . $ofn . '.tgz';
+			   $newfilename = '/data'.$project.'/DAIC/' . $header . '_' . $ofn . '.tgz';
 			   // is this now a new version of the file in DAIC that we would overwrite?
 			   if ( $newfilename != $tgzDAIC[$i] ) {
 			      // no? ok, don't copy this file over to /data/DAIC, we have the wrong timing information here, try with the next one
 			      //echo ("Warning: did not find the correct file in /data/DAIC, should have been: ".$newfilename. ", but got:".$tgzDAIC[$i]."\n");
 			      continue;
 			   }
-			   echo ("Info: did find the same (older) file in /data/DAIC as ".$newfilename." now copy the newer file over to /data/outbox to replace the older file in /data/DAIC");
+			   echo ("Info: did find the same (older) file in /data".$project."/DAIC as ".$newfilename." now copy the newer file over to /data/outbox to replace the older file in /data/DAIC");
 
 		           $fs = glob("/".dirname($tgzQuarantine[$j])."/".$ofn."*");
 		           if ($force) {
 		              foreach ($fs as $f) {
 		                  $path_parts = pathinfo($f);
-		                  $ok = rename($f, "/data/outbox/".$header.'_'.$ofn.".".$path_parts['extension']);
+		                  $ok = rename($f, "/data".$project."/outbox/".$header.'_'.$ofn.".".$path_parts['extension']);
 	  		          if (! $ok) {
-			             echo ("Error moving: ".$f. " to /data/outbox/".$header.'_'.$ofn.".".$path_parts['extension']);
+			             echo ("Error moving: ".$f. " to /data".$project."/outbox/".$header.'_'.$ofn.".".$path_parts['extension']);
 	 		          }			  
 	                     }		      
 		           } else {
