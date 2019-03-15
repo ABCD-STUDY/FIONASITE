056df1b012d3a160776715388013800510161e2f
Hauke Bartsch <HaukeBartsch@gmail.com>
2019-03-15 16:59:59 +0000

    replace split with explode

--- php/stats.php
+++ php/stats.php
@@ -19,7 +19,7 @@ function getSystemMemInfo()
   $data['disk_free_percent'] = round(100.0 * disk_free_space($d) / disk_total_space($d), 2);
   $meminfo = getSystemMemInfo();
   // round(100.0 * intval($meminfo['MemFree']) / intval($meminfo['MemInfo']), 2);
-  $data['memory_free_percent'] = round(100.0 * floatval(split(' ',$meminfo['MemFree'])[0]) / floatval(split(' ',$meminfo['MemTotal'])[0]), 2);
+  $data['memory_free_percent'] = round(100.0 * floatval(explode(' ',$meminfo['MemFree'])[0]) / floatval(explode(' ',$meminfo['MemTotal'])[0]), 2);
   $data['load_avg'] = sys_getloadavg()[0];
   $data['hostname'] = gethostname();
 
